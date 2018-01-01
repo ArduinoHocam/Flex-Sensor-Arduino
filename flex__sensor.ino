8d0512af17c337900bc78ea6ee5006c00a39772e
ArduinoHocam <ArduinoHocam@users.noreply.github.com>
2018-01-01 15:46:01 +0300

    KODLAR(CODES)
    KODLAR(CODES)

---
+++ flex__sensor.ino
@@ -0,0 +1,15 @@
+     /*ARDUINO HOCAM SENSÖRLERİ TANIYALIM#13
+           FLEX SENSÖR KULLANIMI*/
+ int deger;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+deger=analogRead(A0);
+Serial.print("Okunan Deger: ");
+Serial.println(deger);
+delay(50);
+}
